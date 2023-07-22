201d41e348f51c22a35e91379cfa43cef06e88ac
sunli <sunlightsvett@gmail.com>
2023-07-22 18:58:41 +0300

    weather app

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -7,8 +7,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
-import androidx.loader.app.LoaderManager
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +35,31 @@ class MainActivity : AppCompatActivity() {
                 "Your location provider is already turned on.",
                 Toast.LENGTH_SHORT
             ).show()
+
+            Dexter.withContext(this)
+                .withPermissions(android.Manifest.permission.ACCESS_FINE_LOCATION,
+                                 android.Manifest.permission.ACCESS_COARSE_LOCATION,)
+                .withListener(object: MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        report?.let {
+                            if (report.areAllPermissionsGranted()) {
+                                //TODO add requestLocationData()
+                            }
+                            if(report.isAnyPermissionPermanentlyDenied){
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    "You have denied location permission.Please enable them as it is mandatory for the app to work.",
+                                    Toast.LENGTH_SHORT)
+                            }
+                        }
+                    }
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?) {
+                            //TODO - implement showRationalDialogForPermissions()
+                        }
+                    }).onSameThread()
+                    .check()
         }
     }
 
